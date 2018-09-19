915105b0f4db92855d5b5e0b3f1e34ea21a650bc
Natalia Maximo <natalia.maximo@uottawa.ca>
2018-09-19 12:14:27 -0400

    Implementing PointPolar design
    Using PointCP as interface, implemented PointPolar class (previously PointCPPolar) so that it only stores Polar coordinates and calculates the cartesian coordinates on request. Makes an internal conversion from cartesian to polar coordinates if arguments passed are stated to be cartesian in origin

--- Design6/PointCP.java
+++ Design6/PointCP.java
@@ -2,6 +2,7 @@
 // "Object Oriented Software Engineering" and is issued under the open-source
 // license found at www.lloseng.com 
 package Design6;
+
 /**
  * This class contains instances of coordinates in either polar or
  * cartesian format.  It also provides the utilities to convert
@@ -12,48 +13,46 @@ package Design6;
  * @author Dr Timothy C. Lethbridge
  * @version July 2000
  */
-public interface PointCP{
-  
-  //Instance methods **************************************************
- 
-  public double getX();
-  
-  public double getY();
-  
-  public double getRho();
-  
-  public double getTheta();
+public interface PointCP {
+
+    //Interface methods **************************************************
+
+    public double getX();
+
+    public double getY();
+
+    public double getRho();
+
+    public double getTheta();
 
 
-  /**
-   * Calculates the distance in between two points using the Pythagorean
-   * theorem  (C ^ 2 = A ^ 2 + B ^ 2). Not needed until E2.30.
-   *
-   * @param pointA The first point.
-   * @param pointB The second point.
-   * @return The distance between the two points.
-   */
-  public double getDistance(PointCP pointB);
+    /**
+     * Calculates the distance in between two points using the Pythagorean
+     * theorem  (C ^ 2 = A ^ 2 + B ^ 2). Not needed until E2.30.
+     *
+     * @param pointB The second point.
+     * @return The distance between the two points.
+     */
+    public double getDistance(PointCP pointB);
 
-  /**
-   * Rotates the specified point by the specified number of degrees.
-   * Not required until E2.30
-   *
-   * @param point The point to rotate
-   * @param rotation The number of degrees to rotate the point.
-   * @return The rotated image of the original point.
-   */
-  public PointCP rotatePoint(double rotation);
+    /**
+     * Rotates the specified point by the specified number of degrees.
+     * Not required until E2.30
+     *
+     * @param point    The point to rotate
+     * @param rotation The number of degrees to rotate the point.
+     * @return The rotated image of the original point.
+     */
+    public PointCP rotatePoint(double rotation);
 
-  /**
-   * Returns information about the coordinates.
-   *
-   * @return A String containing information about the coordinates.
-   */
-  default String toString()
-  {
-    return "Stored as " + (typeCoord == 'C' 
-       ? "Cartesian  (" + getX() + "," + getY() + ")"
-       : "Polar [" + getRho() + "," + getTheta() + "]") + "\n";
-  }
+    /**
+     * Returns information about the coordinates.
+     *
+     * @return A String containing information about the coordinates.
+     */
+    default String toString() {
+        return "Stored as " + (typeCoord == 'C'
+                ? "Cartesian  (" + getX() + "," + getY() + ")"
+                : "Polar [" + getRho() + "," + getTheta() + "]") + "\n";
+    }
 }
---
+++ Design6/PointPolar.java
@@ -0,0 +1,133 @@
+// This file contains material supporting section 2.9 of the textbook:
+// "Object Oriented Software Engineering" and is issued under the open-source
+// license found at www.lloseng.com 
+package Design6;
+
+/**
+ * This class contains instances of coordinates in either polar or
+ * cartesian format.  It also provides the utilities to convert
+ * them into the other type. It is not an optimal design, it is used
+ * only to illustrate some design issues.
+ *
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @author Dr Timothy C. Lethbridge
+ * @version July 2000
+ */
+
+public class PointPolar {
+    //Instance variables ************************************************
+
+    /**
+     * Contains the current value of X or RHO depending on the type
+     * of coordinates.
+     */
+    private double rho;
+
+    /**
+     * Contains the current value of Y or THETA value depending on the
+     * type of coordinates.
+     */
+    private double theta;
+
+
+    //Constructors ******************************************************
+
+    /**
+     * Constructs a coordinate object, with a type identifier.
+     */
+    public PointPolar(char type, double xOrRho, double yOrTheta) {
+        if (type != 'C' && type != 'P')
+            throw new IllegalArgumentException();
+        if (type == 'C') {
+            this.rho = convertToRho(xOrRho, yOrTheta);
+            this.theta = convertToTheta(xOrRho, yOrTheta);
+        } else {
+            this.rho = xOrRho;
+            this.theta = yOrTheta;
+        }
+    }
+
+
+    //Instance methods **************************************************
+
+
+    public double getX() {
+        return (Math.cos(Math.toRadians(theta)) * rho);
+    }
+
+    public double getY() {
+        return (Math.sin(Math.toRadians(theta)) * rho);
+    }
+
+    public double getRho() {
+        return rho;
+    }
+
+    public double getTheta() {
+        return theta;
+    }
+
+    private double convertToRho(double x, double y) {
+        return (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
+    }
+
+    private double convertToTheta(double x, double y) {
+        return Math.toDegrees(Math.atan2(y, x));
+    }
+
+    /**
+     * Converts Polar coordinates to Cartesian coordinates.
+     */
+    public String getCartesian() {
+        return ("(" + getX() + ", " + getY());
+    }
+
+    /**
+     * Calculates the distance in between two points using the Pythagorean
+     * theorem  (C ^ 2 = A ^ 2 + B ^ 2). Not needed until E2.30.
+     *
+     * @param pointB The second point.
+     * @return The distance between the two points.
+     */
+    public double getDistance(PointCP pointB) {
+        // Obtain differences in X and Y, sign is not important as these values
+        // will be squared later.
+        double deltaX = getX() - pointB.getX();
+        double deltaY = getY() - pointB.getY();
+
+        return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
+    }
+
+    /**
+     * Rotates the specified point by the specified number of degrees.
+     * Not required until E2.30
+     *
+     * @param point    The point to rotate
+     * @param rotation The number of degrees to rotate the point.
+     * @return The rotated image of the original point.
+     */
+    public PointCP rotatePoint(double rotation) {
+        double radRotation = Math.toRadians(rotation);
+        double X = getX();
+        double Y = getY();
+
+        return 0;
+
+        // TODO: Rework the return to match the current form
+    /*
+    return new PointCPPolar('C',
+      (Math.cos(radRotation) * X) - (Math.sin(radRotation) * Y),
+      (Math.sin(radRotation) * X) + (Math.cos(radRotation) * Y));*/
+    }
+
+    /**
+     * Returns information about the coordinates.
+     *
+     * @return A String containing information about the coordinates.
+     */
+    public String toString() {
+        return "Stored as " + (typeCoord == 'C'
+                ? "Cartesian  (" + getX() + "," + getY() + ")"
+                : "Polar [" + getRho() + "," + getTheta() + "]") + "\n";
+    }
+}
