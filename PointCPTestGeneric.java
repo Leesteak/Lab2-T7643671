5b84df81cf31c9ee16ef7ebff87f64dedf661753
Natalia Maximo <natalia.maximo@uottawa.ca>
2018-09-20 19:10:46 -0400

    Added data structures and test class work
    generatePoint method still needs to be implemented
    Removed .idea/ directory and added it to .gitignore

--- PointCPTestGeneric.java
+++ PointCPTestGeneric.java
@@ -2,42 +2,231 @@ import java.util.Scanner;
 
 
 /**
- * Generic test class to base other test classes on.
+ * Test class for all designs
  */
 public class PointCPTestGeneric {
 
-    static final int RUNS = 10000;
+
+	// Testing Constants
+    static final int RUNS = 100;
+    static final int TEST_SIZE = 10000;
+    static final int DESIGN = 4;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("How many tests to run (0 to exit): ");
+        Scanner scanner = new Scanner(System.in); // User input scanner
+        TStack<TestDesign> results; // Used a stack in case of full test
         int runs;
-
-        // TODO: Maybe some better error checking? Idk it doesn't feel like it needs to be any more robust...
+        int design;
+        int testSize;
+        
+        // Design selection
+        System.out.println("Which design do you want to test?");
+        System.out.println("1: Design 2");
+        System.out.println("2: Design 3");
+        System.out.println("3: Design 6");
+        System.out.println("4: Full test (default)");
+        System.out.println("0: Cancel\n");
+        System.out.print(":");
+        
         try {
-            runs = scanner.nextInt();
+        	design = scanner.nextInt();
+        	if (design > 4 || design < 0) throw new Exception(); // Only 4 test options and an exit condition
         }
         catch (Exception e) {
-            System.out.printf("Error occurred, using default value %d... \n");
+        	System.out.printf("Error occurred, using default value %d... \n", DESIGN);
+        	design = DESIGN;
+        }
+        
+        if (design == 0) return;
+        
+        System.out.println("Running test on " + (design = 4) ? ("all designs") : ((design < 3) ? ("Design " + (design+1)) : "Design 6"));
+        
+        
+        // Test size
+        System.out.print("How many tests to run: ");
+        
+        try {
+            runs = scanner.nextInt();
+            if (runs < 1) throw new Exception();
+        }
+        catch (Exception e) {
+            System.out.printf("Error occurred, using default value %d... \n", RUNS);
             runs = RUNS;
         }
-
-        if (runs < 1) return; // Just in case someone wants to be cheeky and use negative values
-
-        // Real talk, why does Java not have unsigned integers? It would kinda solve this whole problem
-        // but in any case, I digress...
-
-        runTests(runs);
+        
+        System.out.print("What should be the test size?")
+        
+        try {
+        	testSize = scanner.nextInt();
+        }
+        catch (Exception e) {
+        	System.out.printf("Error occurred, using default value %d... \n", TEST_SIZE);
+        	testSize = TEST_SIZE;
+        }
+        
+        if (testSize < TEST_SIZE) {
+        	System.out.printf("Test size too small, using default value %d... \n", TEST_SIZE);
+ 			testSize = TEST_SIZE;       	
+        }
+        
+        System.out.printf("Starting %d runs with a test size of %d\n", runs, testSize);
+        
+    	if (design < 4) {
+    		results.push(testDesign(3, runs, size));
+    		results.push(testDesign(2, runs, size));
+    		results.push(testDesign(1, runs, size));
+    	}
+    	else {
+    		results.push(testDesign(design, runs, size));
+    	}
+    	
+    	while (results.peek() != null) { // See all results that have been collected
+    		processTest(results.pop());
+    	}
     }
-
-    private static void runTests(int runs) {
-        // TODO: Define what the tests should actually be and then, you know, code them.
-
-        long[] runTimes = new long[runs]; // Store the times for all the runs so we can calc the median
-        long min, max; // Store the first trial times here, then compare every subsequent trial.
-
-        // Test code starts here
-
-
+    
+    private static void processTest(TestDesign design) {
+    	String[] tests = {"Calculate Distance", "Rotate Point", "Generate Coordinate Cartesian", "Generate Coordinate Polar"}
+    	System.out.println("Data for " + design.getDesignName() + "...");
+    	
+    	// Spit out all test statistics
+    	for (int i = 0; i < tests.length; i++){
+    		String test = tests[i]
+    		System.out.printf("\tTest name: %s\n", test);
+    		// Overall stats
+    		System.out.println("\t\tTotal runtime: " + design.getTotal(test));
+    		System.out.println("\t\tAverage test time: " + design.getAverage(test));
+    		System.out.println("\t\tBest test time: " + design.getTotalBest(test));
+    		System.out.println("\t\tWorst test time: " + design.getTotalWorst(test));
+    		System.out.println();
+    		// Sample Stats
+    		System.out.println("\t\tBest sample time: " + design.getSampleBest(test));
+    		System.out.println("\t\tWorst sample time: " + design.getSampleWorst(test));
+    		System.out.println("\t\tAverage sample time: " + design.getSampleAverage(test));
+    		System.out.println();
+    		System.out.println("\t\tAverage of median times: " + design.getMedianAverage(test));
+    	}
+    }
+    
+    private static TestDesign testDesign(int design, int runs, int size) {
+    	TestDesign results = new TestDesign(design);
+    	long start, end;
+    	
+    	
+    	for (int i = 0; i < runs; i++) {
+    		
+    		// Initialize the appropriate design object
+    		if (design == 1) Design2.PointCP[] points = new Design2.PointCP[size];
+    		else if (design == 2) Design3.PointCP[] points = new Design3.PointCP[size];
+    		else Design6.PointCP[] points = new Design6.PointCP[size];
+    		
+    		// Generate appropriate point instances for each design
+    		for (int j = 0; j < size; j++) {
+    			if (design == 1) { // Design 2
+    				points[i] = new Design2.PointCP('P', generateCoord('P'), generateCoord('P'));
+    			}
+    			else if (design == 2) { // Design 3
+    				points[i] = new Design3.PointCP('C', generateCoord('C'), generateCoord('C'));
+    			}
+    			else { // Design 6
+    				if (i < (size/2)) {
+    					points[i] = new Design6.PointPolar(generateCoord('P'), generateCoord('P'));
+    				}
+    				else {
+    					points[i] = new Design6.PointCartesian(generateCoord('C'), generateCoord('C'));
+    				}
+    			}
+    		}
+    		
+    		// Run all tests and add them to the TestDesign
+    		for (int j = 0; j < size; j++){
+    			results.addTest(testDistance(design, points));
+    			results.addTest(testRotate(design, points));
+    			results.addTest(testCoordinates(design, points, 'C'));
+    			results.addTest(testCoordinates(design, points, 'P'));
+    		}
+    	}
+    	
+    	return results;
+    }
+    
+    private static TestAction testDistance(int design, Object[] points) { // Using generic Object class before casting
+    	if (design == 1) {
+    		Design2.PointCP[] testing = (Design2.PointCP[]) points;
+    		Design2.PointCP testPoint = new Design2.PointCP('C', 0, 0);
+    	}
+    	else if (design == 2){
+    		Design3.PointCP[] testing = (Design3.PointCP[]) points;
+    		Design3.PointCP testPoint = new Design3.PointCP('P', 0, 0);
+    	}
+    	else {
+    		Design6.PointCP[] testing = (Design6.PointCP[]) points;
+    		
+    		/**
+    		* Since PointCartesian and PointPolar share an interface and getDistance uses the general interface for
+    		* its implementation, we can use either PointCartesian or PointPolar for this test. PointCartesian
+    		* was selected for no particular reason.
+    		*/
+    		
+    		Design6.PointCP testPoint = new Design6.PointCartesian(0, 0);
+    	}
+    	
+    	TestAction results = new TestAction("Calculate Distance");
+    	long start, end;
+    	
+    	for (int i = 0; i < testing.length; i++) {
+    		start = System.nanoTime();
+    		testing[i].getDistance(testPoint);
+    		end = System.nanoTime() - start;
+    		results.addTime(end);
+    	}
+    	
+    	return results;
+    }
+    
+    private static TestAction testRotate(int design, Object[] points) { // Using generic Object class before casting
+    	if (design == 1) Design2.PointCP[] testing = (Design2.PointCP[]) points;
+    	else if (design == 2) Design3.PointCP[] testing = (Design3.PointCP[]) points;
+    	else Design6.PointCP[] testing = (Design6.PointCP[]) points;
+    	TestAction results = new TestAction("Rotate Point");
+    	long start, end;
+    	
+    	for (int i=0; i < testing.length; i++) {
+    		start = System.nanoTime();
+    		testing[i].rotatePoint(30); // Rotate 30 degrees
+    		end = System.nanoTime() - start;
+    		results.addTime(end)
+    	}
+    	
+    	return results;
+    }
+    
+    private static TestAction testCoordinates(int design, Object[] points, char type) { // Using generic Object class before casting
+    	if (design == 1) Design2.PointCP[] testing = (Design2.PointCP[]) points;
+    	else if (design == 2) Design3.PointCP[] testing = (Design3.PointCP[]) points;
+    	else Design6.PointCP[] testing = (Design6.PointCP[]) points;
+    	
+    	TestAction results = new TestAction("Generate Coordinate " + (type=='C') ? "Cartesian" : "Polar");
+    	long start, end;
+    	double xOrRho, yOrTheta;
+    	
+    	for (int i = 0; i < testing.length; i++) {
+    		start = System.nanoTime();
+    		if (type == 'C') {
+    			xOrRho = testing[i].getX();
+    			yOrTheta = testing[i].getY();
+    		}
+    		else {
+    			xOrRho = testing[i].getRho();
+    			yOrTheta = testing[i].getTheta();
+    		}
+    		end = System.nanoTime() - start;
+    		results.addTime(end);
+    	}
+    }
+    
+    // TODO: Function to generate random coordinates of cartesian or polar type
+    private static int generateCoord(char type) {
+    	return 0;
     }
 }
---
+++ TestAction.java
@@ -0,0 +1,131 @@
+/**
+* Data structure used to hold data about individual test runs
+*
+*/
+
+public class TestAction {
+	private TStack<long> timesStack;
+	private double median;
+	private long total;
+	private long best;
+	private long worst;
+	private String actionName;
+	
+	
+	/**
+	* Constructor for the class
+	*/
+	
+	public TestAction(String actionName){
+		this.timesStack = new TStack<long>();
+		this.tests = 0;
+		this.worst = 0;
+		this.best = -1;
+		this.actionName = actionName;
+	}
+	
+	/**
+	* Adds time into the stack and collects some stats
+	*
+	* @param time the time to add to the stack
+	*/
+	
+	public void addTime(long time){
+		timesStack.push(time);
+		total += time;
+		if (best == -1 || best > time) best = time;
+		if (worst < time) worst = time;
+		median = null;
+	}
+	
+	/**
+	*
+	*=========================================================================
+	* GETTER FUNCTIONS
+	*=========================================================================
+	*
+	*/
+	
+	/**
+	* Pops the last time from the stack
+	*/
+	
+	public long getLastTime() {
+		return timesStack.pop();
+	}
+	
+	/**
+	* Return the total time for the run
+	*/
+	public long getTotal() {
+		return total;
+	}
+	
+	/**
+	* Return the average time of the run
+	*/
+	
+	public double getAverage() {
+		return (double) total/(double) getTestSize();
+	}
+	
+	/**
+	* Return the number of samples in the run
+	*/ 
+	
+	public int getTestSize() {
+		return timesStack.size();
+	}
+	
+	/**
+	* Return the worst sample time in the run
+	*/
+	
+	public long getWorst() {
+		return worst;
+	}
+	
+	/**
+	* Return the best sample time in the run
+	*/
+	
+	public long getBest() {
+		return best;
+	}
+	
+	/**
+	* Return the name of the test
+	*/
+	
+	public String getTestName() {
+		return actionName;
+	}
+	
+	/**
+	* Return the median value of the run
+	*/
+	
+	public long getMedian() {
+		if (median != null) return median; // Save the median value if no more values have been added
+		int size = timesStack.getSize();
+		long[] values = new long[size];
+		
+		// Load all values into the 'values' array
+		for (int i = 0; i < size; i++) {
+			values[i] = timesStack.pop();
+		}
+		
+		Arrays.sort(values);
+		
+		if (size % 2 == 0) median = ((double) values[size/2] + (double) values[size/2-1]) / 2;
+		else median = (double) values[size/2];
+		
+		
+		// Push values back onto the queue in case more values get added later, to re-calculate the median if need be
+		for (int i = 0; i < size; i++) {
+			timesStack.push(values[i]);
+		}
+		
+		return median;
+	}
+}
